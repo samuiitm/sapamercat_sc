430c2234f79703fc320a93d09ddee8b332f42753
Samuel Cañadas <samuelcanadas2711@gmail.com>
2025-03-14 21:00:37 +0100

    Comentaris afegits al controlador

--- src/Controlador/Controlador.java
+++ src/Controlador/Controlador.java
@@ -92,45 +92,59 @@ public class Controlador {
 
         do {
             try {
-                // Mostra el menú per afegir un producte
+                // Mostrem el menú per afegir productes
                 Vista.mostrarMenuAfegirProducte();
                 opcio = scan.nextInt();
 
                 switch (opcio) {
                     case 1:
                         try {
+                            // Afegir producte d'alimentació
                             Vista.mostrarMissatge("Afegir producte d'alimentació\n");
 
+                            // Demanem el nom del producte
                             Vista.mostrarMissatge("Nom producte: ");
                             String nomAlimentacio = scan.next();
+
+                            // Comprovem que el nom no sigui massa llarg
                             if (nomAlimentacio.length() > 30)
                                 throw new LimitCaractersException("El nom del producte no pot superar els 30 caràcters");
 
+                            // Demanem el preu
                             Vista.mostrarMissatge("Preu: ");
                             float preuAlimentacio = scan.nextFloat();
+
+                            // Comprovem que el preu no sigui negatiu
                             if (preuAlimentacio < 0) throw new NegatiuException("El preu no pot ser negatiu.");
 
+                            // Demanem el codi de barres
                             Vista.mostrarMissatge("Codi barres: ");
                             String codiAlimentacio = scan.next();
 
+                            // Demanem la data de caducitat
                             Vista.mostrarMissatge("Data de caducitat (dd/mm/aaaa): ");
                             LocalDate caducitatAlimentacio = null;
 
+                            // Bucle per assegurar que l'usuari introdueixi bé la data
                             while (caducitatAlimentacio == null) {
                                 try {
                                     String caducitatAlimentacioString = scan.next();
                                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
                                     caducitatAlimentacio = LocalDate.parse(caducitatAlimentacioString, formatter);
+
+                                    // Verifiquem que la data no sigui anterior a avui
                                     DataCaducitatException.verificarDataCaducitat(caducitatAlimentacio);
                                 } catch (DateTimeParseException e) {
-                                    Vista.mostrarMissatge("La data ha de ser en format 'dd/MM/yyyy'. Intenta-ho de nou." + "\n");
+                                    Vista.mostrarMissatge("La data ha de ser en format 'dd/MM/yyyy'. Intenta-ho de nou.\n");
                                     Vista.mostrarMissatge("Data de caducitat (dd/mm/aaaa): ");
                                 } catch (DataCaducitatException e) {
-                                    Vista.mostrarMissatge(e.getMessage() + " Intenta-ho un altre cop." + "\n");
+                                    Vista.mostrarMissatge(e.getMessage() + " Intenta-ho un altre cop.\n");
                                     Vista.mostrarMissatge("Data de caducitat (dd/mm/aaaa): ");
                                     caducitatAlimentacio = null;
                                 }
                             }
+
+                            // Afegim el producte al carret
                             try {
                                 Model.afegirProducteCarret(preuAlimentacio, nomAlimentacio, codiAlimentacio, caducitatAlimentacio);
                                 Vista.mostrarMissatge("Producte afegit al carret.\n\n ");
@@ -144,40 +158,43 @@ public class Controlador {
 
                     case 2:
                         try {
+                            // Afegir producte tèxtil
                             Vista.mostrarMissatge("Afegir producte tèxtil\n");
 
+                            // Demanem el nom del producte
                             Vista.mostrarMissatge("Nom producte: ");
                             String nomTextil = scan.next();
+
+                            // Comprovem la longitud del nom
                             if (nomTextil.length() > 30)
                                 throw new LimitCaractersException("El nom del producte no pot superar els 30 caràcters");
 
-
+                            // Demanem el preu
                             Vista.mostrarMissatge("Preu: ");
                             float preuTextil = scan.nextFloat();
+
+                            // Comprovem que el preu no sigui negatiu
                             if (preuTextil < 0) throw new NegatiuException("El preu no pot ser negatiu.");
 
+                            // Demanem la composició
                             Vista.mostrarMissatge("Composició: ");
                             String composicioTextil = scan.next();
+
+                            // Comprovem la longitud de la composició
                             if (composicioTextil.length() > 30)
                                 throw new LimitCaractersException("La composició del producte tèxtil no pot superar els 30 caràcters");
 
+                            // Demanem el codi de barres
                             Vista.mostrarMissatge("Codi barres: ");
                             String codiTextil = scan.next();
 
-
-
-
-
+                            // Comprovem si ja existeix un producte tèxtil amb aquest codi de barres
                             if (Model.existeixCodiBarres(Model.carretCompra, codiTextil)) {
                                 Vista.mostrarMissatge("No s'ha pogut afegir el producte. Ja existeix un producte amb el mateix codi de barres.\n\n");
                             } else {
                                 try {
-                                    if (Model.existeixCodiBarres(Model.carretCompra, codiTextil)) {
-                                        Vista.mostrarMissatge("Ja existeix un producte tèxtil amb aquest codi de barres al carret.\n");
-                                    } else {
-                                        Model.afegirProducteCarret(preuTextil, nomTextil, codiTextil, composicioTextil);
-                                        Vista.mostrarMissatge("Producte afegit al carret.\n\n");
-                                    }
+                                    Model.afegirProducteCarret(preuTextil, nomTextil, codiTextil, composicioTextil);
+                                    Vista.mostrarMissatge("Producte afegit al carret.\n\n");
                                 } catch (LimitProductesException e) {
                                     Vista.mostrarMissatge("Error: " + e.getMessage() + "\n\n");
                                 }
@@ -189,23 +206,33 @@ public class Controlador {
 
                     case 3:
                         try {
+                            // Afegir producte electrònic
                             Vista.mostrarMissatge("Afegir producte electrònic\n");
 
+                            // Demanem el nom del producte
                             Vista.mostrarMissatge("Nom producte: ");
                             String nomElectronic = scan.next();
+
+                            // Comprovem la longitud del nom
                             if (nomElectronic.length() > 30)
                                 throw new LimitCaractersException("El nom del producte no pot superar els 30 caràcters");
 
+                            // Demanem el preu
                             Vista.mostrarMissatge("Preu: ");
                             float preuElectronic = scan.nextFloat();
+
+                            // Comprovem que el preu no sigui negatiu
                             if (preuElectronic < 0) throw new NegatiuException("El preu no pot ser negatiu.");
 
+                            // Demanem la garantia
                             Vista.mostrarMissatge("Garantia (dies): ");
                             int garantiaElectronic = scan.nextInt();
 
+                            // Demanem el codi de barres
                             Vista.mostrarMissatge("Codi barres: ");
                             String codiElectronic = scan.next();
 
+                            // Afegim el producte al carret
                             try {
                                 Model.afegirProducteCarret(nomElectronic, preuElectronic, garantiaElectronic, codiElectronic);
                                 Vista.mostrarMissatge("Producte afegit al carret.\n\n");
